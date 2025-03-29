d8e51b9c46ca5342c98c0739f14c725b93ad47db
Frakits <xgamerxthegamer@gmail.com>
2025-03-29 19:31:11 +0200

    test

--- songs/oiiaoiia/scripts/script.hx
+++ songs/oiiaoiia/scripts/script.hx
@@ -82,27 +82,30 @@ function postCreate() {
     FlxG.sound.play(Paths.sound("begin"));
 	dad.playAnim("idle alt");
 	dad.animation.curAnim.frameRate = 100;
-	new FlxTimer().start(1.7, function() {
-		dad.playAnim("idle");
-		new FlxTimer().start(1.1, function() {
-            FlxTween.tween(FlxG.camera, {zoom: 1.3}, 8, {ease: FlxEase.sineInOut});
-            defaultCamZoom = 1.3;
-            FlxTween.tween(FlxG.camera.scroll, {y: FlxG.camera.scroll.y + 50}, 14, {ease: FlxEase.sineInOut});
-            FlxG.camera.follow();
-			dad.playAnim("idle alt");
-			dad.animation.curAnim.frameRate = 60;
-			new FlxTimer().start(2.1, function() {
-				dad.playAnim("idle");
-				new FlxTimer().start(.4, function() {
-					oiiaoiia = true;
-					startCountdown();
-					startTimer.cancel();
-					FlxTween.tween(camHUD, {alpha: 1}, 0.1);
-					FlxG.camera.follow();
+	if (chartingMode) oiiaoiia = true;
+	else {
+		new FlxTimer().start(1.7, function() {
+			dad.playAnim("idle");
+			new FlxTimer().start(1.1, function() {
+				FlxTween.tween(FlxG.camera, {zoom: 1.3}, 8, {ease: FlxEase.sineInOut});
+				defaultCamZoom = 1.3;
+				FlxTween.tween(FlxG.camera.scroll, {y: FlxG.camera.scroll.y + 50}, 14, {ease: FlxEase.sineInOut});
+				FlxG.camera.follow();
+				dad.playAnim("idle alt");
+				dad.animation.curAnim.frameRate = 60;
+				new FlxTimer().start(2.1, function() {
+					dad.playAnim("idle");
+					new FlxTimer().start(.4, function() {
+						oiiaoiia = true;
+						startCountdown();
+						startTimer.cancel();
+						FlxTween.tween(camHUD, {alpha: 1}, 0.1);
+						FlxG.camera.follow();
+					});
 				});
 			});
 		});
-	});
+	}
 }
 
 function onStartCountdown(event) {
